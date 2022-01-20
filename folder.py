7e73c25a4c76532ad8ea26c57a23078052c3c9a8
Aorec <95287287+Aorec@users.noreply.github.com>
2022-01-20 09:34:37 +0700

    Create folder.py

---
+++ folder.py
@@ -0,0 +1,44 @@
+import pyautogui
+def gtsport():   
+    pyautogui.write(r'C:\Users\user\Barang\gtsport')
+    pyautogui.press('enter')
+    pyautogui.write(r'"1.jpg" "2.jpg" "3.jpg"')
+def ticarto():
+    pyautogui.write(r'C:\Users\user\Barang\ticarto')
+    pyautogui.press('enter')
+    pyautogui.write(r'"1.jpg" "2.jpg" "3.jpg" "4.jpg"')
+def kaos():
+    pyautogui.write(r'C:\Users\user\Barang\kaos')
+    pyautogui.press('enter')
+    pyautogui.write(r'"11.jpg" "12.jpg" "13.jpg"')
+  #  pyautogui.write(r'"1.jpg" "2.jpg" "3.jpg" "4.jpg" "5.jpg" "6.jpg" "7.jpg"')
+def tas():
+    pyautogui.write(r'C:\Users\user\Barang\tas')
+    pyautogui.press('enter')
+    pyautogui.write(r'"1.jpg" "2.jpg" "3.jpg" "4.jpg"')
+def leopard():
+    pyautogui.write(r'C:\Users\user\Barang\leopard')
+    pyautogui.press('enter')
+    pyautogui.write(r'"1.jpg" "2.jpg" "3.jpg" "4.jpg" "5.jpg"')
+def fngeen():
+    pyautogui.write(r'C:\Users\user\Barang\fngeen')
+    pyautogui.press('enter')
+    pyautogui.write(r'"1.jpg" "2.jpg" "3.jpg" "4.jpg"')
+def quicksilver():
+    pyautogui.write(r'C:\Users\user\Barang\quicksilver')
+    pyautogui.press('enter')
+    pyautogui.write(r'"5.jpg" "6.jpg" "7.jpg"')
+def skymax():
+    pyautogui.write(r'C:\Users\user\Barang\skymax')
+    pyautogui.press('enter')
+  #  pyautogui.write(r'"1.jpg" "2.jpg" "3.jpg" "4.jpg"')
+    pyautogui.write(r'"11.jpg" "12.jpg" "13.jpg"')
+def hushpuppies():
+    pyautogui.write(r'C:\Users\user\Barang\hushpuppies')
+    pyautogui.press('enter')
+    pyautogui.write(r'"1.jpg" "2.jpg" "3.jpg"')
+
+def swissarmy():
+    pyautogui.write(r'C:\Users\user\Barang\swissarmy')
+    pyautogui.press('enter')
+    pyautogui.write(r'"1.jpg" "2.jpg"')
