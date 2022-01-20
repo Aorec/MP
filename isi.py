c0cbc2f330633d84482be3a57ea81651d2937e8f
Aorec <95287287+Aorec@users.noreply.github.com>
2022-01-20 09:33:53 +0700

    Create isi.py

---
+++ isi.py
@@ -0,0 +1,93 @@
+
+daftarisi={
+"1":{ "namabarang":"GT-SPORT","hargadraf":"130000","hargapost":"80000","judul":"(GRATIS ONGKIR + BISA COD) GT-SPORT JAM TANGAN PRIA TREND 2022 (DISKON 50%)", 
+"keterangan": '''* Tampilan: Analog 
+* Movement: Quartz 
+* Dial Shape: Round
+* Material: Silicone
+* Case Diameter: 45mm
+* Case Thickness: 8mm
+* Band length: 20mm (include case)
+* Band Width: 15mm
+'''},
+  
+"2":{ "namabarang":"TICARTO", "hargadraf":"200000", "hargapost":"1300000", "judul":"(GRATIS ONGKIR + BISA COD) JAM TANGAN(TICARTO) TREND 2022", 
+"keterangan":'''- Jam Mewah Import Original 
+- Case Stainless Model elegan
+- Tali Kulit
+- Anti Air sampai kedalaman 30 m
+- Anti Karat
+- Tahan Debu Dan Guncangan
+- Kaca Anti Gores
+- Diameter : 46,2 mm (Gratis Box)
+'''},
+    
+"3":{ "namabarang":"KAOS-LAS","hargadraf":"100000","hargapost":"85000","judul":"KAOS LAS-APPAREL TREND 2022 MURAH KEREN", 
+"keterangan":'''* Bahan Soft Cotton Combed 24s✓
+* Size Chart Lokal (  M . L . XL . XXL ) ✓
+* Rib dan Bahan Sesuai / Macthing ( 1 Pabrik )✓
+* Quality Control Lembut dan Nyaman Dipakai ✓
+* Sablon Polyflex Korea & DTF  ✓
+'''},
+
+"4":{ "namabarang":"TAS-WEISTBAG","hargadraf":"100000","hargapost":"100000","judul":"(BISA COD) WEIST BAG BELI 1 DAPAT 3", 
+"keterangan":'''DAPATKAN SEGERA SEBELUM KEHABISAN
+KARENA STOK TERBATAS YUK DI ORDER BURUAN'''},
+    
+"5":{ "namabarang":"LEOPARD","hargadraf":"130000","hargapost":"80000","judul":"(GRATIS ONGKIR + BISA COD) JAM TANGAN (LEOPARD) TREND 2022", 
+"keterangan":'''->LEOPARD ORIGINAL IMPORT<-
+-Case Silicone Model elegan
+-Tali Strap PU Material
+-Anti Air up to 50 m
+-Anti Karat
+-Tahan Debu Dan Guncangan
+-Kaca Anti Gores
+-Diameter : 41 mm 
+-(Gratis Box)'''},
+
+"6":{ "namabarang":"FNGEEN","hargadraf":"250000","hargapost":"170000","judul":"(GRATIS ONGKIR + BISA COD) FNGEEN LUXURY SPORT JAM TANG PRIA KRONOGRAF", 
+"keterangan":'''* Merek: FNGEEN
+* Bahan tali: Stainless Steel
+* Bahan kasing: logam
+* Bahan gesper: stainless steel
+* Tahan air: 30m Tahan air biasa. Tidak untuk berenang dan menghindari menyentuh air panas dan uap
+* Fungsi: Kalender, Bercahaya, Tampilan Minggu
+ ORI PAKET:
+ 1 x jam tangan +box +Buku Pegangan Merek +Label Merek'''},
+
+"7":{ "namabarang":"QUICKSILVER","hargadraf":"250000","hargapost":"130000","judul":"(GRATIS ONGKIR + BISA COD) JAM TANGAN QUICKSILVER LIMITED EDITION", 
+"keterangan":'''* Merek: QUICKSILVER
+* Bahan tali: RUBBER HALUS
+* Bahan kasing: SILICONE
+* Bahan gesper: STAINLESS STEEL
+* Diameter : 50 MM
+* Tahan air: 50 M AIR BIASA
+* Fitur : DATE AND LIGHT
+* Gratis ongkir untuk wilayah pulau jawa saja'''},
+
+"8":{ "namabarang":"SKYMAX","hargadraf":"300000","hargapost":"150000","judul":"(GRATIS ONGKIR + BISA COD) SKYMAX JAM TANGAN PRIA / WANITA TREND 2022", 
+"keterangan":'''* Merek: SKYMAX
+* Bahan tali: PU SINTETIS
+* Bahan kasing: SILICONE
+* Bahan gesper: STAINLESS STEEL
+* Tahan air: 50m Tahan air biasa.
+* Gratis ongkir untuk wilayah pulau jawa saja'''},
+
+"9":{ "namabarang":"HUSH-PUPPIES","hargadraf":"120000","hargapost":"90000","judul":"(GRATIS ONGKIR + BISA COD) HUSH-PUPPIES JAM TANGAN WANITA TREND 2022", 
+"keterangan":'''* MEREK: HUSH-PUPPIES
+* DIAMETER : 3,7 CM
+* Bahan kasing: MIKA AND PLASTIK
+* BAHAN TALI : STRAP RUBER
+* CHRONO DAN TANGGAL AKTIF
+* Gratis ongkir untuk wilayah pulau jawa saja'''},
+    
+"10":{ "namabarang":"SWISS-ARMY","hargadraf":"250000","hargapost":"150000","judul":"(GRATIS ONGKIR + BISA COD) SWISS-ARMY JAM TANGAN PRIA KEREN 2022", 
+"keterangan":'''* Merek: SWISS-ARMY
+* Bahan tali: LEATHER
+* Bahan kasing : STAINLESS STEEL
+* Tahan air: 42 m Tahan air biasa
+* Diameter : 45 mm
+* Gratis ongkir untuk wilayah pulau jawa saja
+'''},
+    
+}
