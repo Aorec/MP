d5c96d909cd647c585b8b6764d4c3137decbfda0
Aorec <95287287+Aorec@users.noreply.github.com>
2022-01-20 09:35:07 +0700

    Create akun.py

---
+++ akun.py
@@ -0,0 +1,13 @@
+akun1={
+"1":{"email":"cintakayla91@gmail.com", "pass":"ARFan1308", "nama":"Cinta Kayla Azahra","cookies":"cookies/cinta.pkl" },
+"2":{"email":"085708384854",           "pass":"ARFan1308", "nama":"Enjang Cahyana","cookies":"cookies/enjang.pkl"},
+"3":{"email":"cindaulia768@gmail.com", "pass":"ARFan1308", "nama":"Cindy Aulia ","cookies":"cookies/cindy.pkl"},
+"4":{"email":"enjangcahyana25@gmail.com",  "pass":"ARFan1308", "nama":"Muhammad Arfan ","cookies":"cookies/arfan.pkl"},
+"5":{"email":"cahyana.enjang1@yahoo.com",  "pass":"ARFan1308", "nama":"Risa Alatas","cookies":"cookies/risa.pkl"},
+"6":{"email":"nindanarnia@gmail.com",  "pass":"ARFan1308", "nama":"Rahma Rahadian","cookies":"cookies/rahma.pkl"},
+"7":{"email":"stya642@yahoo.com",  "pass":"ARFan1308", "nama":"Zaina Abidin","cookies":"cookies/zaina.pkl"},
+"8":{"email":"liliskartini359@yahoo.com",  "pass":"ARFan1308", "nama":"Windi Eka Tea","cookies":"cookies/windi.pkl"},
+"9":{"email":"katerinakatrin86@yahoo.com",  "pass":"ARFan1308", "nama":"Katerina Katrin","cookies":"cookies/katerina.pkl"},
+"10":{"email":"cheisyakaligis@yahoo.com",  "pass":"azka@2020", "nama":"Cheisya Kaligis","cookies":"cookies/cheisya.pkl"},
+
+}
