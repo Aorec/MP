dcb480f1b33bed7cdd4c71f28f7d60f32e2d1df6
Aorec <95287287+Aorec@users.noreply.github.com>
2022-01-20 09:32:58 +0700

    Create modul.py

---
+++ modul.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome import options
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+import unittest
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+import pyautogui
+import os
+import pickle
+import  json
